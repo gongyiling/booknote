70834e01322e1f60bc5d432fd62cc11fac3f6f1e
gongyiling3468@163.com <gongyiling3468@163.com>
2012-07-03 23:38:44 +0800

    modify publish.sh

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
 
+PROJ_ROOT=..
+PUB_ROOT=/home/web/booknote
+TMP=/tmp
+cp -r $PROJ_ROOT/back $TMP/
+cp -r $PROJ_ROOT/static $TMP/
+cp -r $PROJ_ROOT/templates $TMP/
+if [ ! -d $TMP/proto ]; then
+	mkdir $TMP/proto
+fi
+cp -r $PROJ_ROOT/proto/*.py $TMP/proto/
+sudo su - web
+cp -r $TMP/back $PUB_ROOT/
+cp -r $TMP/static $PUB_ROOT/
+cp -r $TMP/templates $PUB_ROOT/
+if [ ! -d $PUB_ROOT/proto ]; then
+	mkdir $PUB_ROOT/proto
+fi
+cp -r $TMP/proto/*.py $PUB_ROOT/proto/
+
